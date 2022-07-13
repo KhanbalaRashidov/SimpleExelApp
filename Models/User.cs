ad67af527bb30ee1f77af31cd1190c31ea1b2fd3
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2022-07-13 15:23:55 +0400

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleExelApp.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
